cad7dea86d445342efa1349a0c90e80815936a26
Pablo Zuil <zupablo3@gmail.com>
2025-02-27 11:10:10 +0100

    Arreglos en la clases añadiendo LocalDateTime a las fechas de nacimiento e incorporación

--- src/main/kotlin/models/Jugador.kt
+++ src/main/kotlin/models/Jugador.kt
@@ -1,5 +1,7 @@
 package org.example.models
 
+import java.time.LocalDateTime
+
 class Jugador (
     val posicion: String,
     val dorsal: Int,
@@ -10,8 +12,8 @@ class Jugador (
     override val id: Int,
     override val nombre: String,
     override val apellidos: String,
-    override val fechaNacimiento: String,
-    override val fechaIncorporacion: String,
+    override val fechaNacimiento: LocalDateTime,
+    override val fechaIncorporacion: LocalDateTime,
     override val salario: Double,
     override val pais: String
 ): Empleado(id, nombre, apellidos, fechaNacimiento, fechaIncorporacion, salario, pais)
