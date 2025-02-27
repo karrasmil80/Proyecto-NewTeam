191f923b8ea9f0d9e868f0a233ce09b03a7f39d6
PabloDLF <pablodelafuente.dls@gmail.com>
2025-02-27 03:05:37 +0100

    En este commit subo la creación de la carpeta "models" y en su interior he creado el primer archivo "Empleado.kt"

---
+++ src/main/kotlin/models/Empleado.kt
@@ -0,0 +1,13 @@
+package org.example.models
+
+import java.sql.Date
+
+class Empleado (
+    val id: Int,
+    val nombre: String,
+    val apellidos: String,
+    val fechaNacimiento: String,
+    val fechaIncorporacion: String,
+    val salario: Double,
+    val pais: String
+)
