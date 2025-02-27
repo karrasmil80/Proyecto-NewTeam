9f8b961bfee82b2d2bcab5e2b046c5ae4106dc2c
Pablo Zuil <zupablo3@gmail.com>
2025-02-27 11:10:10 +0100

    Arreglos en la clases añadiendo LocalDateTime a las fechas de nacimiento e incorporación

--- src/main/kotlin/models/Empleado.kt
+++ src/main/kotlin/models/Empleado.kt
@@ -1,13 +1,14 @@
 package org.example.models
 
 import java.sql.Date
+import java.time.LocalDateTime
 
 open class Empleado (
     open val id: Int,
     open val nombre: String,
     open val apellidos: String,
-    open val fechaNacimiento: String,
-    open val fechaIncorporacion: String,
+    open val fechaNacimiento: LocalDateTime,
+    open val fechaIncorporacion: LocalDateTime,
     open val salario: Double,
     open val pais: String
 )
